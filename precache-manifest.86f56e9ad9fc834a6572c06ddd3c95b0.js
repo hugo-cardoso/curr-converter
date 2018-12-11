668cde6c79662ba720fa9add6b294f73d45003b0
tunadao1 <hugorafaelmc@gmail.com>
2018-12-11 20:29:37 -0200

    Updates

---
+++ precache-manifest.86f56e9ad9fc834a6572c06ddd3c95b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a001c0b74512d099ae0d",
+    "url": "/curr-converter/static/js/runtime~main.a001c0b7.js"
+  },
+  {
+    "revision": "515c52d6bc2fcbec374d",
+    "url": "/curr-converter/static/js/main.515c52d6.chunk.js"
+  },
+  {
+    "revision": "482587be3ca03c188643",
+    "url": "/curr-converter/static/js/1.482587be.chunk.js"
+  },
+  {
+    "revision": "515c52d6bc2fcbec374d",
+    "url": "/curr-converter/static/css/main.32fbfb5e.chunk.css"
+  },
+  {
+    "revision": "7124d4057ebe0698eabaa5fb43b8b9ca",
+    "url": "/curr-converter/index.html"
+  }
+];
